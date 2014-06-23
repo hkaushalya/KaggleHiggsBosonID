39a62a9638d402558b23c54bcf8c4e70ea254050
samantha <samantha@cern.ch>
2014-06-22 22:07:09 -0500

    OOB ctree first attempt with primary vars only.

---
+++ rawcode/ctree_oob.R
@@ -0,0 +1,17 @@
+getwd()
+df <- read.csv('data/training.csv')
+str(df)
+col.names <- names(df)
+der.vars <- col.names[grep('DER_', col.names)]
+pri.vars <- col.names[grep('PRI_', col.names)]
+target <- 'Label'
+sb.weight <- 'Weight'
+hist(df$DER_mass_MMC)
+hist(df$DER_mass_vis)
+#boxplot(df[,pri.vars])
+#plot(df[,pri.vars])
+library(party)
+df.prim.only <- data.frame(cbind(df[,pri.vars], df$Label))
+
+ct <- ctree(df.prim.only$df.Label ~ ., data = df.prim.only)
+plot(ct)
